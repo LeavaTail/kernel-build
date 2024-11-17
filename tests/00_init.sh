cfca1751008a3382814d4f4fde60f61a8eb4e6fe
LeavaTail <starbow.duster@gmail.com>
2024-11-17 13:45:54 +0900

    CI: Add quite mode in get kernel tree
    Signed-off-by: LeavaTail <starbow.duster@gmail.com>

--- tests/00_init.sh
+++ tests/00_init.sh
@@ -7,7 +7,7 @@ file=(`cat lts_kernel.txt`)
 
 for lts in "${file[@]}"; do
 	if [ ! -e ${lts}.tar.xz ]; then
-		wget https://cdn.kernel.org/pub/linux/kernel/v`echo ${lts:6:1}`.x/${lts}.tar.xz	
+		wget -q https://cdn.kernel.org/pub/linux/kernel/v`echo ${lts:6:1}`.x/${lts}.tar.xz	
 	fi
 	tar xf ${lts}.tar.xz
 	chmod 777 -R ${lts}
